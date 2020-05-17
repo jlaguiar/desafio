4c52b7766892d6d5f607f2058b60f41aa67d2fa6
joao luiz <takayama585@gmail.com>
2020-05-17 19:55:00 -0400

    identando e mudando ordem de methods

--- app/src/views/proposta/maior-nota/MaiorNota.vue
+++ app/src/views/proposta/maior-nota/MaiorNota.vue
@@ -52,36 +52,26 @@
             await this.preencherLista()
             this.dialogCarregar = false
         },
-        computed: {
-
-        },
         methods: {
             ...mapActions(['bucarPropostasNotas']),
-            verMais() {
-            },
-            async preencherLista() {
-                this.listaPropostasNota = await this.bucarPropostasNotas()
-                this.parseDataParaInteiro()
-                this.listaPropostas = await this.ordenarListaNotaMaior()
-            },
-            async ordenarListaNotaMaior(){
-                await this.listaPropostasNota.sort(function(valor1, valor2) {
-                    if(valor1.nota > valor2.nota){
+            async ordenarListaNotaMaior() {
+                await this.listaPropostasNota.sort(function (valor1, valor2) {
+                    if (valor1.nota > valor2.nota) {
                         return -1
-                    }else if(valor1.nota < valor2.nota){
+                    } else if (valor1.nota < valor2.nota) {
                         return 1
-                    }else{
-                        if(valor1.preco < valor2.preco){
+                    } else {
+                        if (valor1.preco < valor2.preco) {
                             return -1
-                        }else if(valor1.preco > valor2.preco){
+                        } else if (valor1.preco > valor2.preco) {
                             return 1
-                        }else{
+                        } else {
                             debugger
-                            if(valor1.dataCadastroInteiro < valor2.dataCadastroInteiro){
+                            if (valor1.dataCadastroInteiro < valor2.dataCadastroInteiro) {
                                 return -1
-                            }else if(valor1.dataCadastroInteiro > valor2.dataCadastroInteiro){
+                            } else if (valor1.dataCadastroInteiro > valor2.dataCadastroInteiro) {
                                 return 1
-                            }else{
+                            } else {
                                 return 0
                             }
                         }
@@ -89,15 +79,23 @@
                 })
                 return this.listaPropostasNota
             },
-            parseDataParaInteiro(){
-                this.listaPropostasNota.forEach(function(element,index){
-                    let projetoModificado  = this[index].dataCadastro.replace(/\D/g, ' ')
+            parseDataParaInteiro() {
+                this.listaPropostasNota.forEach(function (element, index) {
+                    let projetoModificado = this[index].dataCadastro.replace(/\D/g, ' ')
                     projetoModificado = projetoModificado.split(' ')
                     let projetoJuncao = projetoModificado[2]
-                    projetoJuncao = projetoJuncao + projetoModificado[1]+ projetoModificado[0] + projetoModificado[3] + projetoModificado[4] + projetoModificado[5]
+                    projetoJuncao = projetoJuncao + projetoModificado[1] + projetoModificado[0] + projetoModificado[3] + projetoModificado[4] + projetoModificado[5]
                     this[index].dataCadastroInteiro = parseInt(projetoJuncao)
                 }, this.listaPropostasNota);
-            }
+            },
+            async preencherLista() {
+                this.listaPropostasNota = await this.bucarPropostasNotas()
+                this.parseDataParaInteiro()
+                this.listaPropostas = await this.ordenarListaNotaMaior()
+            },
+            verMais() {
+            },
+
 
         }
     }
